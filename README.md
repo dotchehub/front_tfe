982601f207fd139cf36899935a239eb33924fcb1
dotched <dotchecode@outlook.be>
2022-12-07 17:30:59 +0100

    react app

---
+++ README.md
@@ -0,0 +1,1 @@
+# Vinder_FE
---
+++ vinder_app/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Its almost 2pm and we have not done anything yet. go call!!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
